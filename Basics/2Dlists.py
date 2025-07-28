9079fffa4daeef6e1d87bc42ae3083988d9c6b0f
kalyankoya05 <140375090+kalyankoya05@users.noreply.github.com>
2025-07-28 08:26:11 +0000

    Add

--- Basics/2Dlists.py
+++ Basics/2Dlists.py
@@ -1,9 +1,18 @@
-drinks = ['coffee','soda','tea']
-dinner = ['pizza','soup','biryani']
-dessert = ['icecream','cake']
+# drinks = ['coffee','soda','tea']
+# dinner = ['pizza','soup','biryani']
+# dessert = ['icecream','cake']
+
+# food = [drinks,dinner,dessert]
+
+# print(food)
+# print(food[1])
+# print(food[1][2])
+
+drinks = ["coke","thumsup","Maaza"]
+dinner = ["biryani","rice","mutton"]
+dessert = ["Cake","Icecream"]
 
 food = [drinks,dinner,dessert]
 
 print(food)
-print(food[1])
 print(food[1][2])
--- Basics/ControlStatements.py
+++ Basics/ControlStatements.py
@@ -11,8 +11,26 @@
 #     print(i,end="")
 
 
+# for i in range(1,20):
+#     if i == 13:
+#         pass
+#     else:
+#         print(i)
+
+# while True:
+#     name = input("Enter your name: ")
+#     if(name !=""):
+#         break
+
+# phone_number = "987-654-3210"
+# for i in phone_number:
+#     if(i=="-"):
+#         continue
+#     print(i,end="")
+
+
 for i in range(1,20):
-    if i == 13:
+    if i == 10:
         pass
     else:
         print(i)
